72acffcdc2086dad3325066c750f3d3ca96ae1eb
Ruan jianing <43362789+Ruanjn@users.noreply.github.com>
2019-11-09 14:40:02 +0800

    Problem of cabinet

---
+++ Problem7_23.java
@@ -0,0 +1,33 @@
+package 数组作业;
+
+public class Problem7_23 {
+	
+	public static boolean turnboolean(boolean b) {
+		if(b==true) {
+			return false;
+		}else { 
+			return true;
+		}
+	}
+	public static void main(String[] args) {
+		boolean[] cabinet=new boolean[100];
+		
+		for(int i=0;i<100;i++) {
+			cabinet[i]=false;
+		}
+		
+		for(int i=0;i<100;i++) {
+			for(int j=i;j<100;j+=i+1) {
+				cabinet[j]=turnboolean(cabinet[j]);
+			}
+		}
+		
+		for(int i=0;i<100;i++) {
+			if(cabinet[i]==true) {
+				System.out.print(i+" ");
+			}
+		}
+		
+	}
+	
+}
