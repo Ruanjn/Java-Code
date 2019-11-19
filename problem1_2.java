4b2237a5bb5022b22c5868a815e7d1cbbec92511
Ruan jianing <43362789+Ruanjn@users.noreply.github.com>
2019-11-19 22:30:04 +0800

    第一单元

---
+++ problem1_2.java
@@ -0,0 +1,14 @@
+package 第一单元;
+
+public class problem1_2 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");	
+	}
+
+}
